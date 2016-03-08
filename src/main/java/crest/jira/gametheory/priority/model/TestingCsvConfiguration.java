53877324e00ccdac6ffeb728dc889950ecc150e6
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-03-08 16:49:01 +0000

    Recording tester activity

--- src/main/java/crest/jira/gametheory/priority/model/TestingCsvConfiguration.java
+++ src/main/java/crest/jira/gametheory/priority/model/TestingCsvConfiguration.java
@@ -2,56 +2,56 @@ package crest.jira.gametheory.priority.model;
 
 public class TestingCsvConfiguration {
 
-  public static final String RELEASE = "Release";
-  public static final String TESTER = "Tester";
+    public static final String RELEASE = "Release";
+    public static final String TESTER = "Tester";
 
-  public static final String DEVELOPER_PRODUCTIVITY = "Developer Productivity";
-  public static final String TESTER_PRODUCTIVITY = "Tester Productivity";
-  public static final String DEVELOPER_PRODUCTIVITY_RATIO = "Developer Productivity Ratio";
+    public static final String DEVELOPER_PRODUCTIVITY = "Developer Productivity";
+    public static final String TESTER_PRODUCTIVITY = "Tester Productivity";
+    public static final String DEVELOPER_PRODUCTIVITY_RATIO = "Developer Productivity Ratio";
 
-  public static final String TIME_FRAME_INFLATION = "Release Inflation";
+    public static final String TIME_FRAME_INFLATION = "Release Inflation";
 
-  public static final String ISSUES_REPORTED = "Issues Reported";
-  public static final String POSSIBLE_INFLATIONS = "Possible Inflations";
-  public static final String INFLATION_RATIO = "Inflation Ratio";
-  public static final String SEVERE_ISSUES_FOUND = "Severe Issues";
-  public static final String DEFAULT_ISSUES = "Default Issues";
-  public static final String NON_SEVERE_ISSUES_REPORTED = "Non-Severe Issues";
-  public static final String NON_SEVERE_ISSUES_FOUND = "Non-Severe Issues Found";
+    public static final String ISSUES_REPORTED = "Issues Reported";
+    public static final String POSSIBLE_INFLATIONS = "Possible Inflations";
+    public static final String INFLATION_RATIO = "Inflation Ratio";
+    public static final String SEVERE_ISSUES_FOUND = "Severe Issues";
+    public static final String DEFAULT_ISSUES = "Default Issues";
+    public static final String NON_SEVERE_ISSUES_REPORTED = "Non-Severe Issues";
+    public static final String NON_SEVERE_ISSUES_FOUND = "Non-Severe Issues Found";
 
-  public static final String EXTERNAL_INFLATION = "External Inflation";
-  public static final String NEXT_RELEASE_FIXES = "Next Release Fixes";
+    public static final String EXTERNAL_INFLATION = "External Inflation";
+    public static final String NEXT_RELEASE_FIXES = "Next Release Fixes";
 
-  public static final String EXTERNAL_SEVERITY = "External Severity";
-  public static final String SEVERE_FIX_PROBABILITY = "Severe Fix Probability";
-  public static final String NON_SEVERE_FIX_PROBABILITY = "Non Severe Fix Probability";
-  public static final String EXPECTED_SEVERE_FIXES = "Expected Severe Fixes";
-  public static final String EXPECTED_INFLATED_FIXES = "Expected Inflated Fixes";
-  public static final String EXPECTED_NON_SEVERE_FIXES = "Expected Non Severe Fixes";
-  public static final String EXPECTED_FIXES = "Expected Fixes";
-  public static final String STRATEGY_INDEX = "Strategy Index for Player ";
-  public static final String PAYOFF_VALUE = "Payoff Value for Player ";
-  public static final String STRATEGY_VALUE = "Strategy Value for Player ";
-  public static final String AVG_INFLATION_RATIO = "Inflation Ratio (mean)";
-  public static final String MED_INFLATION_RATIO = "Inflation Ratio (med)";
-  public static final String VAR_INFLATION_RATIO = "Inflation Ration (var)";
-  public static final String SEVERE_ISSUES_REPORTED = "Severe Issues Reported";
-  public static final String SEVERE_RATIO_REPORTED = "Severe Ratio Reported";
-  public static final String NON_SEVERE_RATIO_REPORTED = "Non Severe Ratio Reported";
-  public static final String TESTER_PARTICIPATION = "Tester Participation";
-  public static final String RELEASE_SEVERITY_RATIO = "Release Severity Ratio";
-  public static final String TESTER_INFLATION_SLOPE = "Tester Inflation Slope";
-  public static final String SUCCESS_RATIO = "Success Ratio";
-  public static final String REJECTED_ISSUES = "Rejected Issues";
-  public static final String RELEASE_REJECTION = "Release Rejection";
-  public static final String NUMBER_OF_TESTERS = "Number of Testers";
-  public static final String DEFAULT_ISSUES_FOUND = "Default Issues Found";
-  public static final String DEFAULT_ISSUES_REPORTED = "Default Issues Reported";
-  public static final String NONSEVERE_INFLATION_RATIO = "Non-Severe Inflation Ratio";
-  public static final String DEFAULT_INFLATION_RATIO = "Default Inflation Ratio";
-  public static final String TIMEFRAME_INFLATION_RATIO = "Time Frame Inflation Ratio";
-  public static final String SEVERE_RELEASE_FIXES = "Severe Release Fixes";
-  public static final String DEFAULT_RELEASE_FIXES = "Default Release Fixes";
-  public static final String NONSEVERE_RELEASE_FIXES = "Non Severe Release Fixes";
-
+    public static final String EXTERNAL_SEVERITY = "External Severity";
+    public static final String SEVERE_FIX_PROBABILITY = "Severe Fix Probability";
+    public static final String NON_SEVERE_FIX_PROBABILITY = "Non Severe Fix Probability";
+    public static final String EXPECTED_SEVERE_FIXES = "Expected Severe Fixes";
+    public static final String EXPECTED_INFLATED_FIXES = "Expected Inflated Fixes";
+    public static final String EXPECTED_NON_SEVERE_FIXES = "Expected Non Severe Fixes";
+    public static final String EXPECTED_FIXES = "Expected Fixes";
+    public static final String STRATEGY_INDEX = "Strategy Index for Player ";
+    public static final String PAYOFF_VALUE = "Payoff Value for Player ";
+    public static final String STRATEGY_VALUE = "Strategy Value for Player ";
+    public static final String AVG_INFLATION_RATIO = "Inflation Ratio (mean)";
+    public static final String MED_INFLATION_RATIO = "Inflation Ratio (med)";
+    public static final String VAR_INFLATION_RATIO = "Inflation Ration (var)";
+    public static final String SEVERE_ISSUES_REPORTED = "Severe Issues Reported";
+    public static final String SEVERE_RATIO_REPORTED = "Severe Ratio Reported";
+    public static final String NON_SEVERE_RATIO_REPORTED = "Non Severe Ratio Reported";
+    public static final String TESTER_PARTICIPATION = "Tester Participation";
+    public static final String RELEASE_SEVERITY_RATIO = "Release Severity Ratio";
+    public static final String TESTER_INFLATION_SLOPE = "Tester Inflation Slope";
+    public static final String SUCCESS_RATIO = "Success Ratio";
+    public static final String REJECTED_ISSUES = "Rejected Issues";
+    public static final String RELEASE_REJECTION = "Release Rejection";
+    public static final String NUMBER_OF_TESTERS = "Number of Testers";
+    public static final String DEFAULT_ISSUES_FOUND = "Default Issues Found";
+    public static final String DEFAULT_ISSUES_REPORTED = "Default Issues Reported";
+    public static final String NONSEVERE_INFLATION_RATIO = "Non-Severe Inflation Ratio";
+    public static final String DEFAULT_INFLATION_RATIO = "Default Inflation Ratio";
+    public static final String TIMEFRAME_INFLATION_RATIO = "Time Frame Inflation Ratio";
+    public static final String SEVERE_RELEASE_FIXES = "Severe Release Fixes";
+    public static final String DEFAULT_RELEASE_FIXES = "Default Release Fixes";
+    public static final String NONSEVERE_RELEASE_FIXES = "Non Severe Release Fixes";
+    public static final String TESTER_REPORTED_ISSUES= "Tester Reported Issues";
 }
